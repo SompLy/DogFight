1787aa65cb706264d9e4ecd20178d74fdd82fb79
Edvin Öhrström <edviohrs@playgroundsquad.com>
2024-05-29 13:57:19 +0530

    Updated controls for aiming and attacks
    I thought alot on how the aiming would make sense on a keyboard only solution and decided on this current control scheme.

--- assets/scripts/PlayerControls.cs
+++ assets/scripts/PlayerControls.cs
@@ -10,4 +10,10 @@ public partial class PlayerControls : Resource
 	public string MoveRight { get; set; } = "p1_right";
 	[Export]
 	public string Jump { get; set; } = "p1_up";
+	[Export]
+	public string Attack { get; set; } = "p1_attack";
+	[Export]
+	public string AimLeft { get; set; } = "p1_aimLeft";
+	[Export]
+	public string AimRight { get; set; } = "p1_aimRight";
 }
