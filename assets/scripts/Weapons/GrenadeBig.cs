c85b15a27189c8dbf84086b9cf01b11610285dbe
Edvin Öhrström <edviohrs@playgroundsquad.com>
2024-05-30 19:12:49 +0530

    Changed grenade to workd with particle
    I have to do some funky stuff and delete the weapon after the explosion perticle has happend from the removeParticle script

--- assets/scripts/Weapons/GrenadeBig.cs
+++ assets/scripts/Weapons/GrenadeBig.cs
@@ -6,7 +6,8 @@ public partial class GrenadeBig : Throwable
 	private Label _label;
 	private Timer _timer;
 	private Map _map;
-
+	private GpuParticles2D _particleInstance;
+	
 	public override void _Ready()
 	{
 		base._Ready();
@@ -35,12 +36,13 @@ public partial class GrenadeBig : Throwable
 		_map.Explosion( Position, 30 );
 		_label.Visible                          = false;
 		GetNode<Sprite2D>( "Sprite2D" ).Visible = false;
-		//GetNode<AnimationPlayer>("Explosion").Visable = true;
-		//GetNode<AnimationPlayer>("Explosion").Play("default");
-		QueueFree();
+		InstantiateParticle();
 	}
-
-	public void _OnExplosionAnimationFinished()
+	public void InstantiateParticle( )
 	{
+		PackedScene scene = GD.Load<PackedScene>( "res://assets/ParticleEffects/particle_explosion.tscn" );
+		_particleInstance = ( GpuParticles2D )scene.Instantiate();
+		AddChild( _particleInstance );
+		_particleInstance.Emitting = true;
 	}
 }
