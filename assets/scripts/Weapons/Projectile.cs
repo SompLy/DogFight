73d539e66f92041278fe8c16bbd551ddaa1ba321
Edvin Öhrström <edviohrs@playgroundsquad.com>
2024-05-30 20:17:05 +0530

    Fix for bazooka normal issue
    Now checking on both the velocity.X and veloccity.Y if statements to ensure that Hit will always be set to true.

--- assets/scripts/Weapons/Projectile.cs
+++ assets/scripts/Weapons/Projectile.cs
@@ -5,7 +5,7 @@ public partial class Projectile : Node2D
 {
 	private Map _map;
 
-	private const float GRAVITY = 0.1f;
+	private const float GRAVITY = 0.4f;
 	public Vector2 Dir;
 
 	public bool Hit = false;
@@ -29,18 +29,18 @@ public partial class Projectile : Node2D
 	private void ApplyVelocityWithCollisions ()
 	{
 		Vector2 velocity = Dir;
-
+			
 		while ( Mathf.Abs( velocity.Y ) > 0 )
 		{
 			var newPosition = Position +
 			                  Vector2.Down * Mathf.Sign( velocity.Y ) * Mathf.Min( Mathf.Abs( velocity.Y ), 1.0f );
 			var normal = _map.CollisionNormalPoint( newPosition );
 			velocity.Y -= Mathf.Min( 1.0f, Mathf.Abs( velocity.Y ) ) * Mathf.Sign( velocity.Y );
-
+			
 			if ( normal == Vector2.One )
 				break;
 			
-			if ( normal != Vector2.Zero )
+			if ( normal != Vector2.Zero ) 
 				Hit = true;
 			
 			LookAt( newPosition );
@@ -57,6 +57,9 @@ public partial class Projectile : Node2D
 			if ( normal == Vector2.One )
 				break;
 
+			if ( normal != Vector2.Zero ) 
+				Hit = true;
+			
 			Position = newPosition;
 			LookAt( newPosition );
 		}
