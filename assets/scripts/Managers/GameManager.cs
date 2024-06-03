da1a7c1c65e8233a2c65442cecc87127d1818a6f
Edvin Öhrström <edviohrs@playgroundsquad.com>
2024-06-03 13:54:41 +0530

    GameManager update
    * Added score handling
    * Function to reload map

--- assets/scripts/Managers/GameManager.cs
+++ assets/scripts/Managers/GameManager.cs
@@ -8,17 +8,37 @@ public partial class GameManager : Node
 		PvP,
 		PvAi
 	}
-
+	
 	public EGameMode GameMode = EGameMode.PvP;
 	
 	public bool UseMouse = false;
 	public int CurrentRound = 0;
-
+	public int Player1Score = 0;
+	public int Player2Score = 0;
+	
 	public override void _Input( InputEvent @event )
 	{
 		if ( @event is InputEventKey { Keycode: Key.M, Pressed: true } )
 		{
 			UseMouse = !UseMouse;
 		}
+	}
+
+	public void PlayerDeath( int playerIndex )
+	{
+		switch ( playerIndex )
+		{
+			case 0:
+				Player2Score++;
+				break;
+			case 1:
+				Player1Score++;
+				break;
+		}
+		ReloadMap();
+	}
+	private void ReloadMap()
+	{
+		GetTree().ChangeSceneToPacked( ResourceLoader.Load<PackedScene>( "res://main.tscn" ) );
 	}
 }
