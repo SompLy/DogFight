333501aa2af2a6bc974457a85e0e053d574431ba
Edvin Öhrström <edviohrs@playgroundsquad.com>
2024-06-04 21:16:09 +0530

    Main menu update
    * Set as main scene
    * Added button for tournament mode

--- assets/scripts/Menu.cs
+++ assets/scripts/Menu.cs
@@ -4,22 +4,42 @@ using System;
 public partial class Menu : Control
 {
 	private PackedScene _gameScene;
+	private PackedScene _player;
+	private PackedScene _enemy;
+	private PackedScene _map;
 	private GameManager _gameManager;
+
 	public override void _Ready()
 	{
-		_gameScene = ResourceLoader.Load<PackedScene>( "res://main.tscn" );
-		_gameManager = GetNode<GameManager>("/root/GameManager");
+		_gameManager = GetNode<GameManager>( "/root/GameManager" );
+		_gameScene   = ResourceLoader.Load<PackedScene>( "res://main.tscn" );
+		_player      = ResourceLoader.Load<PackedScene>( "res://player_controller.tscn" );
+		_enemy       = ResourceLoader.Load<PackedScene>( "res://enemy_controller.tscn" );
+		_map         = ResourceLoader.Load<PackedScene>( "res://map.tscn" );
 	}
+
 	public void _on_player_vs_player_pressed()
 	{
 		_gameManager.GameMode = GameManager.EGameMode.PvP;
+		_gameManager.UseMouse = false;
+
 		GetTree().ChangeSceneToPacked( _gameScene );
 	}
+
 	public void _on_player_vs_ai_pressed()
 	{
-		_gameManager.GameMode = GameManager.EGameMode.PvAi;
+		_gameManager.GameMode = GameManager.EGameMode.PvAISingle;
+		_gameManager.UseMouse = true;
 		GetTree().ChangeSceneToPacked( _gameScene );
 	}
+
+	public void _on_player_vs_ai_tournament_pressed()
+	{
+		_gameManager.GameMode = GameManager.EGameMode.PvAITournament;
+		_gameManager.UseMouse = true;
+		GetTree().ChangeSceneToPacked( _gameScene );
+	}
+
 	public void _on_exit_game_pressed()
 	{
 		GetTree().Quit();
