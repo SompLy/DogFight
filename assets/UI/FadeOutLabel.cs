31a8d95e835bd189828b584e84ec091e58942973
Edvin Öhrström <edviohrs@playgroundsquad.com>
2024-06-11 14:13:03 +0530

    Score label
    At the start of every round there is a label that shows name of opponent and the current score that fades away.

---
+++ assets/UI/FadeOutLabel.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class FadeOutLabel : Control
+{
+	private Label _label;
+	private GameManager _gameManager;
+	float _alpha = 1.0f;
+	public override void _Ready()
+	{
+		_label = GetNode<Label>( "Label" );
+		_gameManager = GetNode<GameManager>( "/root/GameManager" );
+		_label.Text = "Dog Vs. " + GetParent().GetNode<Controller>( "Player2" ).Nickname + "\n" + 
+		              _gameManager.Player1Score + " | " + _gameManager.Player2Score;
+	}
+
+	public override void _Process(double delta)
+	{
+		_alpha -= 0.2f * ( float )delta;
+		_label.AddThemeColorOverride( "font_color", new Color( 1, 1, 1, _alpha ) );
+	}
+}
