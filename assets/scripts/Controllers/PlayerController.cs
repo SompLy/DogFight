dfe434fb27a545a441d309d49724e5e52f94d89b
Edvin Öhrström <edviohrs@playgroundsquad.com>
2024-06-11 14:17:26 +0530

    Mouse controls updated
    * Mouse can now use all weapons

--- assets/scripts/Controllers/PlayerController.cs
+++ assets/scripts/Controllers/PlayerController.cs
@@ -5,15 +5,38 @@ public partial class PlayerController : Controller
 {
 	[Export] public  PlayerControls  Controls;
 	
+	public Node2D RotationPoint;
+	public Node2D DirectionSprite;
 	public override void _Ready()
 	{
 		base._Ready();
+			
+		RotationPoint   = GetNode<Node2D>( "RotationPoint" );
+		DirectionSprite = GetNode<Sprite2D>( "RotationPoint/DirectionSprite" );
+		if ( _GameManager.GameMode != GameManager.EGameMode.PvP )
+			DirectionSprite.Visible = false;
 	}
 	public override void _Input( InputEvent @event )
 	{
 		// Mouse & keyboard
 		if ( @event is InputEventMouseButton { Pressed: true } && _GameManager.UseMouse )
-			InstantiateGrenade( GetGlobalMousePosition() - GlobalPosition + new Vector2( 0, -12 ) );
+		{
+			switch ( CurrentWeapon )
+			{
+				case EWeapon.Bazooka:
+					InstantiateBazooka( GetGlobalMousePosition() - GlobalPosition + new Vector2( 0, -12 ) 
+						* AttackPowerBazooka * 0.5f );
+					break;
+				case EWeapon.Grenade:
+					InstantiateGrenade( GetGlobalMousePosition() - GlobalPosition + new Vector2( 0, -12 ) );
+					break;
+				case EWeapon.Molotov:
+					//InstantiateMolotov( ( _directionSprite.GlobalPosition - _rotationPoint.GlobalPosition ) * _attackPower );
+					break;
+				default:
+					break;
+			}
+		}
 		// Keyboard only
 		if ( Input.IsActionPressed( Controls.Attack ) )
 		{
@@ -59,7 +82,6 @@ public partial class PlayerController : Controller
 	public override void _Process(double delta)
 	{
 		base._Process( delta );
-
 
 	}
 	public override void _PhysicsProcess(double delta)
