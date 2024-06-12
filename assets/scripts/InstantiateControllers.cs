9ae68dac518dda6dd7df7f63b08d66a527b2fa86
Edvin Öhrström <edviohrs@playgroundsquad.com>
2024-06-12 20:37:02 +0530

    cut content

--- assets/scripts/InstantiateControllers.cs
+++ assets/scripts/InstantiateControllers.cs
@@ -24,7 +24,7 @@ public partial class InstantiateControllers : Node
 		// Player 1
 		PlayerController p1Instance = ( PlayerController )_player.Instantiate();
 		p1Instance.Name          = "Player1";
-		p1Instance.Nickname		 = "Player 1";
+		p1Instance.Nickname		 = "Cool Dog";
 		p1Instance.Controls      = ( PlayerControls )GD.Load( "res://assets/Resource/p1_Controls.tres" );
 		p1Instance.PlayerTexture = GD.Load<CompressedTexture2D>( "res://assets/sprites/Dog_Collie.png" );
 		p1Instance.AddToCamera   = true;
@@ -47,8 +47,7 @@ public partial class InstantiateControllers : Node
 		Enemy1Controller enemy1Instance = null;
 		Enemy2Controller enemy2Instance = null;
 		Enemy3Controller enemy3Instance = null;
-		Enemy4Controller enemy4Instance = null;
-		Enemy5Controller enemy5Instance = null;
+
 
 		switch ( _gameManager.CurrentTournamentOpponent )
 		{
@@ -56,56 +55,34 @@ public partial class InstantiateControllers : Node
 				// Granat Göran
 				enemy1Instance = ( Enemy1Controller )_enemy.Instantiate();
 				enemy1Instance.Name          = "Player2";
-				enemy1Instance.Nickname      = "Granat Göran";
+				enemy1Instance.Nickname      = "Gränslösa Granat-Göran";
 				enemy1Instance.PlayerTexture = GD.Load<CompressedTexture2D>( "res://assets/sprites/Dog_Golden.png" );
 				enemy1Instance.AddToCamera   = true;
 				enemy1Instance.PlayerIndex   = 1;
-				enemy1Instance.Health        = 3;
+				enemy1Instance.Health        = 5;
 				enemy1Instance.Position      = new Vector2( 900.0f, 100.0f );
 				break;
 			case 1:
 				// Bosse Bazooka
 				enemy2Instance = ( Enemy2Controller )_enemy.Instantiate();
 				enemy2Instance.Name          = "Player2";
-				enemy2Instance.Nickname		 = "Bazooka Bosse";
+				enemy2Instance.Nickname		 = "Bångstyriga Bazooka Benny";
 				enemy2Instance.PlayerTexture = GD.Load<CompressedTexture2D>( "res://assets/sprites/Dog_Golden.png" );
 				enemy2Instance.AddToCamera   = true;
 				enemy2Instance.PlayerIndex   = 1;
-				enemy2Instance.Health        = 3;
+				enemy2Instance.Health        = 5;
 				enemy2Instance.Position      = new Vector2( 900.0f, 100.0f );
 				break;
 			case 2:
-				// Molotov Melker
+				// Örjan den överlägsna
 				enemy3Instance = ( Enemy3Controller )_enemy.Instantiate();
 				enemy3Instance.Name          = "Player2";
-				enemy3Instance.Nickname		 = "Molotov Melker";
-				enemy3Instance.PlayerTexture = GD.Load<CompressedTexture2D>( "res://assets/sprites/Dog_Golden.png" );
+				enemy3Instance.Nickname		 = "Örjan den Överlägsne";
+				enemy3Instance.PlayerTexture = GD.Load<CompressedTexture2D>( "res://assets/sprites/Dog_Demon.png" );
 				enemy3Instance.AddToCamera   = true;
 				enemy3Instance.PlayerIndex   = 1;
-				enemy3Instance.Health        = 3;
+				enemy3Instance.Health        = 10;
 				enemy3Instance.Position      = new Vector2( 900.0f, 100.0f );
-				break;
-			case 3:
-				// Örjan den överlägsna
-				enemy4Instance = ( Enemy4Controller )_enemy.Instantiate();
-				enemy4Instance.Name          = "Player2";
-				enemy4Instance.Nickname		 = "Örjan den Överlägsne";
-				enemy4Instance.PlayerTexture = GD.Load<CompressedTexture2D>( "res://assets/sprites/Dog_Golden.png" );
-				enemy4Instance.AddToCamera   = true;
-				enemy4Instance.PlayerIndex   = 1;
-				enemy4Instance.Health        = 4;
-				enemy4Instance.Position      = new Vector2( 900.0f, 100.0f );
-				break;
-			case 4:
-				// Demon Dog
-				enemy5Instance = ( Enemy5Controller )_enemy.Instantiate();
-				enemy5Instance.Name          = "Player2";
-				enemy5Instance.Nickname		 = "Demon Dog";
-				enemy5Instance.PlayerTexture = GD.Load<CompressedTexture2D>( "res://assets/sprites/Dog_Golden.png" );
-				enemy5Instance.AddToCamera   = true;
-				enemy5Instance.PlayerIndex   = 1;
-				enemy5Instance.Health        = 5;
-				enemy5Instance.Position      = new Vector2( 900.0f, 100.0f );
 				break;
 		}
 
@@ -145,12 +122,6 @@ public partial class InstantiateControllers : Node
 						break;
 					case 2:
 						GetParent().CallDeferred( "add_child", enemy3Instance );
-						break;
-					case 3:
-						GetParent().CallDeferred( "add_child", enemy4Instance );
-						break;
-					case 4:
-						GetParent().CallDeferred( "add_child", enemy5Instance );
 						break;
 				}
 				break;
