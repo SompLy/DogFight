53bced49a2e4a4481876be5d40416095f4da6cf9
Edvin Öhrström <edviohrs@playgroundsquad.com>
2024-06-13 20:57:30 +0530

    Spin attack
    * Added epic spin attack as a special move for the last enemy.

--- assets/scripts/Controllers/Enemies/Enemy3Controller.cs
+++ assets/scripts/Controllers/Enemies/Enemy3Controller.cs
@@ -11,11 +11,26 @@ public partial class Enemy3Controller : EnemyController
 		BazookaShort,
 		BazookaLong,
 		Grenade,
-		Spin
+		BazookaSpin
 	}
 	private EState _state = EState.Walking;
 	private bool _inAttackRangeLong;
 	private RandomNumberGenerator _randomNumberGenerator = new RandomNumberGenerator();
+	// Spin
+	private float _spinTime;
+	private float _spinJumpWeight;
+	private float _spinDegrees;
+	private Vector2 _spinPosition;
+	public Node2D RotationPoint;
+	public Node2D DirectionSprite;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		RotationPoint   = GetNode<Node2D>( "RotationPoint" );
+		DirectionSprite = GetNode<Sprite2D>( "RotationPoint/DirectionSprite" );
+	}
+
 	public override void _Process(double delta)
 	{
 		base._Process( delta );
@@ -42,7 +57,15 @@ public partial class Enemy3Controller : EnemyController
 				if ( AttackTimer1 >= 0 )
 					break;
 				
+				// Set state
 				_state = _inAttackRangeLong ? EState.BazookaLong : EState.BazookaShort;
+
+				if ( _randomNumberGenerator.RandiRange( 0, 5000 ) == 0 )
+				{
+					Walk      = 0;
+					_state    = EState.BazookaSpin;
+					_spinTime = 5.0f;
+				}
 				
 				break;
 			case EState.BazookaShort:
@@ -80,11 +103,48 @@ public partial class Enemy3Controller : EnemyController
 				_state = EState.Walking;
 				
 				break;
-			case EState.Spin:
+			case EState.BazookaSpin:
+				InternalVelocity = Vector2.Zero;
+				if( _spinJumpWeight < 0.2f )
+				{
+					GD.Print( _spinJumpWeight );
+					_spinJumpWeight += (float)delta * 0.1f;
+					Position        =  LerpVector2( Position, new Vector2( Position.X, 300.0f ), _spinJumpWeight);
+					_spinPosition   =  Position;
+					break;
+				}
+				
+				if ( _spinJumpWeight >= 0.2f && _spinTime > 0.0f)
+				{
+					Position  =  _spinPosition;
+					_spinTime -= (float)delta;
+						
+					if ( AttackTimer1 <= 0.0f )
+					{
+						InstantiateBazooka( ( DirectionSprite.GlobalPosition - RotationPoint.GlobalPosition ) *
+						                    AttackPowerBazooka * 1.5f, PlayerIndex );
+						RotationPoint.RotationDegrees -= 9.0f;
+						AttackTimer1                  =  0.05f;
+					}
+				}
+				// Switch state and reset
+				if ( _spinTime <= 0.0f )
+				{
+					_spinJumpWeight = 0.0f;
+					_state          = EState.Walking;
+				}
 				
 				break;
 			default:
 				break;
 		}
+	}
+	private Vector2 LerpVector2( Vector2 start, Vector2 end, float percentage )
+	{
+		Vector2 tempVec = new Vector2();
+		tempVec.X = start.X + ( end.X - start.X ) * percentage;
+		tempVec.Y = start.Y + ( end.Y - start.Y ) * percentage;
+		
+		return tempVec;
 	}
 }
